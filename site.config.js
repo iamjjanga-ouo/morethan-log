43ccd5fe790b618cd23882e3eee85e10c7335a06
iamjjanga-ouo <namils147@kakao.com>
2025-03-14 22:47:22 +0900

    fix : recover projects component

--- site.config.js
+++ site.config.js
@@ -11,10 +11,10 @@ const CONFIG = {
     instagram: "",
   },
   projects: [
-    // {
-    //   name: `morethan-log`,
-    //   href: "https://github.com/iamjjana-ouo/morethan-log",
-    // },
+    {
+      name: `hackutm`,
+      href: "https://hackutm.com",
+    },
   ],
   // blog setting (required)
   blog: {
